c24fe19c5f3722315bbada2a0687c2179f88d050
JAAM4494 <arielarias7@estudiantec.cr>
2016-05-19 01:05:19 -0600

    Se renombran clases y se documenta código

--- app/src/main/java/com/app/preparatec2/About_Screen.java
+++ app/src/main/java/com/app/preparatec2/About_Screen.java
@@ -1,7 +1,5 @@
 package com.app.preparatec2;
 
-import com.app.preparatec2.R;
-
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -10,30 +8,29 @@ import android.widget.TextView;
 public class About_Screen extends Activity {
 
 	public static final String copyright  = "\u00a9";
-	TextView labelLeyenda, labelAuthors;
-	String leyenda;
-	String authors;
+
+	private TextView legendLabel, authorsLabel;
+	private String legendString, authorsString;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_about);
 
-		labelLeyenda = (TextView)findViewById(R.id.textView1);
-		labelAuthors = (TextView)findViewById(R.id.textView2);
+		legendLabel = (TextView)findViewById(R.id.textView1);
+		authorsLabel = (TextView)findViewById(R.id.textView2);
 
-		leyenda = "PreparaTEC es una herramienta informática que permite al estudiante prepararse para la prueba de aptitud académica del "
+		legendString = "PreparaTEC es una herramienta informática que permite al estudiante prepararse para la prueba de aptitud académica del "
 				+ "Tecnológico de Costa Rica. La aplicación contendrá información basada en las guías de preparación, con el fin de brindar "
 				+ "al estudiante una noción sobre la naturaleza de los ítems de dicha prueba.";
 		
-		authors =  "Desarrolladores: \n Jose Ariel Arias Méndez \n Edwin Vásquez Leiva \n José Pablo Fernández \n" + 
+		authorsString =  "Desarrolladores: \n Jose Ariel Arias Méndez \n Edwin Vásquez Leiva \n José Pablo Fernández \n" +
 		" Luis Alejandro Corrales \n Gabriela Delgado Quesada \n" + copyright + " 2014-2015 PreparaTEC \n Todos los derechos reservados";
 
-		labelLeyenda.setTextColor(Color.parseColor("#414042"));
-		labelLeyenda.setText(leyenda);
+		legendLabel.setTextColor(Color.parseColor("#414042"));
+		legendLabel.setText(legendString);
 		
-		labelAuthors.setTextColor(Color.parseColor("#414042"));
-		labelAuthors.setText(authors);
-
+		authorsLabel.setTextColor(Color.parseColor("#414042"));
+		authorsLabel.setText(authorsString);
 	}
 }
